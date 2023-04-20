e4ae650e2eee16b2e6e79f7337084fe607c88b22
raghav <raghavsingh31@outlook.com>
2023-04-20 21:38:46 +0530

    learned about semigroup

---
+++ src/main/scala/com/singhraghav/catspractice/TCSemigroup.scala
@@ -0,0 +1,30 @@
+package com.singhraghav.catspractice
+
+object TCSemigroup extends App {
+
+  import cats.Semigroup
+  import  cats.syntax.semigroup._
+  def reduceThings[T : Semigroup](things: List[T]): T = things.reduce(_ |+| _)
+  case class Expense(id: Long, amount: Double)
+
+  implicit val expenseSemiGroup: Semigroup[Expense] =
+    Semigroup.instance((exp1, exp2) => Expense(id = Math.max(exp1.id, exp2.id), amount = exp1.amount + exp2.amount))
+
+  val expenses = List(Expense(1, 1), Expense(2, 12))
+
+  val expenseOption = List(Option(Expense(1, 1)), None)
+
+  println(reduceThings(expenses))
+
+  import cats.instances.option._
+  println(reduceThings(expenseOption))
+
+  // extension methods from semigroup
+
+
+  val finalExpense = Expense(1, 2) |+| Expense(3, 10)
+
+  println(finalExpense)
+
+
+}
