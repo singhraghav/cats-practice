868a34b91c1554026167a44b1c4ec9a1e4e24586
raghav <raghavsingh31@outlook.com>
2023-05-04 22:22:15 +0530

    learned readers

---
+++ src/main/scala/com/singhraghav/catspractice/MyReader.scala
@@ -0,0 +1,25 @@
+package com.singhraghav.catspractice
+
+import cats.Id
+
+object MyReader extends App {
+
+
+  case class Configuration(emailReplyTo: String)
+
+  case class EmailService(emailReplyTo: String) {
+    def sendEmail(address: String, contents: String) = s"From: $emailReplyTo; to: $address >>> $contents"
+  }
+
+  def getOrderStatus(username: String) = "Shipped"
+
+  import cats.data.Reader
+  def emailUser(user: String): Id[String] = {
+    Reader.apply[Configuration, EmailService](configuration => EmailService(configuration.emailReplyTo))
+      .map(emailService => emailService.sendEmail(user, "someContent"))
+      .run(Configuration("raghav"))
+  }
+
+  println(emailUser("jina"))
+
+}
