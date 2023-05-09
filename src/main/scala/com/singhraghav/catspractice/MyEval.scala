c374a7a4683f00ad2eabfbd1f6fe86e2d2280955
raghav <raghavsingh31@outlook.com>
2023-05-09 21:25:32 +0530

    learned eval

---
+++ src/main/scala/com/singhraghav/catspractice/MyEval.scala
@@ -0,0 +1,19 @@
+package com.singhraghav.catspractice
+
+import scala.:+
+
+object MyEval extends App {
+
+  import cats.Eval
+
+  def defer[T](eval: => Eval[T]): Eval[T] = Eval.later(()).flatMap(_ => eval)
+
+  def reverseEval[T](list: List[T]): Eval[List[T]] =
+    if(list.isEmpty) Eval.now(list)
+    else Eval.defer(reverseEval(list.tail).map{_ :+ list.head})
+
+  defer(Eval.now{println("Hello"); 42}).value
+
+  println(reverseEval(List(1,2,3)).value)
+
+}
