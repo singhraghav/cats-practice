ff5250546dd94917fcbb4867fe083d8ff3bf08cf
raghav <raghavsingh31@outlook.com>
2023-05-15 22:07:35 +0530

    learned foldable

---
+++ src/main/scala/com/singhraghav/catspractice/MyFoldable.scala
@@ -0,0 +1,21 @@
+package com.singhraghav.catspractice
+
+import cats.Monoid
+import cats.implicits.catsSyntaxSemigroup
+
+object MyFoldable extends App {
+
+  object ListExercises {
+    def map[A, B](list: List[A])(f: A => B): List[B] =
+      list.foldLeft(List.empty[B])((acc, curr) => f(curr) :: acc).reverse
+
+    def combineAll[A](list: List[A])(implicit monoid: Monoid[A]): A = list.foldLeft(monoid.empty)((acc, curr) => acc |+| curr)
+  }
+
+  import cats.Foldable
+
+  import cats.instances.list._
+
+  Foldable[List]
+
+}
